a317e3ea0a0a7454fc440ddc2a667dab38eb43c8
rifatmuhammad04 <rifatmuhammad04@gmail.com>
2026-09-19 15:20:51 +0700

    Update greetings in main function

--- PRAK002-2610817210003-Muhammad Rifat.C
+++ PRAK002-2610817210003-Muhammad Rifat.C
@@ -2,7 +2,7 @@
 
 int main() {
     printf("Selamat Pagi, Muhammad Rifat\n");
-    printf("Selamat Pagi, Muhammad Rifat\n");
-    printf("Selamat Pagi, Muhammad Rifat\n");
+    printf("Selamat siang, Muhammad Rifat\n");
+    printf("Selamat malam, Muhammad Rifat\n");
     return 0;
 }
